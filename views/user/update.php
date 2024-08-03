bc6afca50e94b5510f9730319a23406fb0369bb0
Dmitry Khlystov <dkhlystov@gmail.com>
2024-08-03 22:21:36 +1000

    Some fixes

--- views/user/update.php
+++ views/user/update.php
@@ -9,7 +9,7 @@ $this->params['breadcrumbs'] = [
 ];
 
 ?>
-<h1><?= Html::encode($this->title); ?></h1>
+<h1><?= Html::encode($this->title) ?></h1>
 <?= $this->render('form', [
     'model' => $model,
 ]) ?>
